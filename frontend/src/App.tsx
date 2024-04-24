1ee642d5a651dda95894d1c75c4b5abe6ee6f2e9
Jasmina <jasminajerkovic007@gmail.com>
2024-04-25 00:19:22 +0200

    feature: implemented the button component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Button } from './components/buttonComponent/Button';
 import { Textfield } from './components/textFieldComponent/textfield';
 
 function App() {
@@ -7,7 +8,8 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Textfield headerText='email' size='medium' placeholder='mail@adress.com' />} />
+          <Route path="/b" element={<Button label='Klikni' size='small' onClick={() => { }} />} />
+          <Route path="/t" element={<Textfield headerText='email' size='medium' placeholder='mail@adress.com' />} />
           <Route path="/hello" element={<p>Hello</p>} />
         </Routes>
       </Router>
