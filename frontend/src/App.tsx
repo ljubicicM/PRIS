2f5a9528263a717e78d5e4828febd15bc3601e4d
Marko Ljubičić <markoljubicic77@gmail.com>
2024-04-24 23:06:40 +0200

    feature: added global css parameters and implemented textfield component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Textfield } from './components/textFieldComponent/textfield';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<p>Hi</p>} />
+          <Route path="/" element={<Textfield headerText='email' size='medium' placeholder='mail@adress.com' />} />
           <Route path="/hello" element={<p>Hello</p>} />
         </Routes>
       </Router>
