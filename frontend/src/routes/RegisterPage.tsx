46a31f8277aaedf073c1d723b6c6980e4154b979
Marko Ljubičić <107189662+ljubicicM@users.noreply.github.com>
2024-05-08 20:26:52 +0200

    feature: added UI changes for register page and its components

--- frontend/src/routes/RegisterPage.tsx
+++ frontend/src/routes/RegisterPage.tsx
@@ -3,22 +3,34 @@ import { Button } from '../components/buttonComponent/Button';
 import { PasswordField } from '../components/passwordFieldComponent/password';
 import { Textfield } from '../components/textFieldComponent/textfield';
 import { RadioButtons } from '../components/radioButtonComponent/RadioButtons';
+import './styles/loginRegisterPage.css';
 
 export const RegisterPage = () => {
     const [password, setPassword] = useState("");
     const [repeatPassword, setRepeatPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [username, setUsername] = useState("");
+
+    const checkEmail = () => {
+        return email.includes('@') && email.includes('.')
+    }
     const checkPassword = () => {
         return (password === repeatPassword && password.length > 0)
     }
 
     return (
-        <div>
-            <h1>Register</h1>
-            <RadioButtons labels={["Turista", "Vodič"]} name='accountType' />
-            <Textfield size='medium' placeholder="email" headerText="Email" />
-            <PasswordField value={password} setValue={setPassword} placeholder="************" header="Password" />
-            <PasswordField value={repeatPassword} setValue={setRepeatPassword} placeholder="************" header="Confirm Password" />
-            <Button label="Register" onClick={() => alert('Register')} />
+        <div className='register-container-outer'>
+            <div className='register-container'>
+                <h1>Register</h1>
+                <RadioButtons labels={["Turista", "Vodič"]} name='accountType' />
+                <Textfield value={username} setValue={setUsername} size='medium' placeholder="username" headerText="User Name" />
+                <Textfield value={phoneNumber} setValue={setPhoneNumber} size='medium' placeholder="phone number" headerText="Phone Number" />
+                <Textfield value={email} setValue={setEmail} size='medium' placeholder="email" headerText="Email" />
+                <PasswordField value={password} setValue={setPassword} placeholder="************" header="Password" />
+                <PasswordField value={repeatPassword} setValue={setRepeatPassword} placeholder="************" header="Confirm Password" />
+                <Button isEnabeld={checkPassword() && checkEmail()} label="Register" onClick={() => alert('Register')} size='large' />
+            </div>
         </div>
     );
 }
