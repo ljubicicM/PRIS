66163a3e1a99f712ffe26ec21e2a6149646d426b
Marko Ljubičić <107189662+ljubicicM@users.noreply.github.com>
2024-06-02 22:59:29 +0200

    feature: added dropdown component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { RegisterPage } from './routes/RegisterPage';
 import { LoginPage } from './routes/LoginPage';
+import { CRSearchByPage } from './routes/CRSearchByPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/cr/searchby" element={<CRSearchByPage />} />
         </Routes>
       </Router>
     </div>
